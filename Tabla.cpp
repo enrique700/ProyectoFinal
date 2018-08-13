b15f13f42086ebdb7ddcc377334f2dfc667b439c
Luis Enrique Galan Fajardo <40571407+enrique700@users.noreply.github.com>
2018-08-13 10:34:54 -0400

    Subida inicial proyecto final

---
+++ Tabla.cpp
@@ -0,0 +1,33 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "conio.h"
+
+main () { 
+
+int a=0,b;
+
+ printf (" \n Numero a Multiplicar > ");   
+
+ scanf ("%d",&b);
+
+for(a=1;a<=20;a++)
+
+printf("\n %d x %d = %d",b,a,b*a);
+
+
+
+ getch () ; 
+
+/*Luis Enrique Galan Fajardo 13-EIST-1-154
+
+int a=0;
+
+for(a=1;a<=12;a++)
+
+printf("\n 8 x %d = %d",a,8*a);
+
+*/
+
+
+
+} 
